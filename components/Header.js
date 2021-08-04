48ebf8be8c0aa0e3d6e5c2813b518c93b77c2eb1
James <67014970+JLoweDev@users.noreply.github.com>
2021-08-04 17:34:50 +0100

    Header and component wrapped in container tag

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
         <header>
-            
+            <div className="container">
+                <Link href='/'>
+                    <h2>Hopeful Coder</h2>
+                </Link>
+            </div>
         </header>
     )
 }
