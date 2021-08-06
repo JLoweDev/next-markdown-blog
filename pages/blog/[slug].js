e38990751c2bce0322dcf7602ce66e5a475188b9
James <67014970+JLoweDev@users.noreply.github.com>
2021-08-06 15:30:57 +0100

    [slug].js created

---
+++ pages/blog/[slug].js
@@ -0,0 +1,17 @@
+export default function PostPage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export async function getStaticPaths() {
+
+}
+
+export async function getStaticProps() {
+    return {
+        props: {}
+    }
+}
