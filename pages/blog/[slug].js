258beb7d7a1a85312006ba158d31f2ab2805903f
James <67014970+JLoweDev@users.noreply.github.com>
2021-08-06 15:44:22 +0100

    staticpaths added

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+
 export default function PostPage() {
     return (
         <div>
-
+            post
         </div>
     )
 }
 
 export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join('posts'))
 
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.md', '')
+        }
+    }))
+
+    console.log(paths)
+
+    return {
+        paths,
+        fallback: false
+    }
 }
 
 export async function getStaticProps() {
